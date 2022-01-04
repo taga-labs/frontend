a4226d653d0e8b36fc79825f650f391444f4812a
n-variable <nvariable@protonmail.com>
2022-01-04 16:23:24 -0500

    backup

---
+++ src/landing/index.js
@@ -0,0 +1,7 @@
+import LandingHome from './pages/home';
+import LandingDocs from './pages/docs';
+
+export {
+    LandingHome,
+    LandingDocs
+}
---
+++ src/landing/pages/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class LandingHome extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
