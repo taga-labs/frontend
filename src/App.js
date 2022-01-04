2d3d604d727c0a93e260ac5550780046cef700ba
n-variable <nvariable@protonmail.com>
2022-01-04 16:10:57 -0500

    restructuring

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
+
+import {
+    LandingHome
+} from '';
+
+let App;
+
+let uri = window.location.host;
+
+App = class AppRouter extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return(
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={} />
+                </Routes>
+            </BrowserRouter>
+        )
+    }
+}
+
+App = class LandingRouter extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return(
+            <BrowserRouter>
+                <Routes>
+                    <Route />
+                </Routes>
+            </BrowserRouter>
+        )
+    }
 }
 
 export default App;
