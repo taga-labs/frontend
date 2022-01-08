5ab2c1196bcf8cbf66cfc68ba078e752b5d704d8
n-variable <nvariable@protonmail.com>
2022-01-08 15:22:27 -0500

    two more graphics remaining then moving onto docs and about

--- src/pages/landing/pages/home.js
+++ src/pages/landing/pages/home.js
@@ -2,6 +2,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Fontawesome icons
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLongArrowAltDown, faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons';
+
 // Components
 import Nav from '../components/nav';
 
@@ -12,8 +16,8 @@ import '../../../assets/styles/typography.css';
 
 // Images
 import MainVectorGraphic from '../../../assets/img/taga-main-image.svg';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLongArrowAltDown, faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons';
+import SharePartitioningGraphic from '../../../assets/img/taga-partitioning-image.svg';
+import TokenGenerationGraphic from '../../../assets/img/taga-tokens-image.svg';
 
 
 export default class LandingHome extends React.Component {
@@ -44,10 +48,64 @@ export default class LandingHome extends React.Component {
                 </div>
                 <div className="container content">
                     <div className="container content centered">
+                        <img src={SharePartitioningGraphic} className="secondary image" />
                         <div className="container content centered vertical">
-                            <a className="text tagline">Private Equity<br /><span className="text keyword tagline">Democratized.</span></a>
+                            <div style={{paddingLeft: '5vw'}}>
+                                <a className="text tagline">Partition your equity.</a>
+                                <p className="text tagline smaller">As an existing shareholder, set aside equity to list in our ecosystem, and in its place a token will be deployed.</p>
+                                <p className="text paragraph main" style={{width: '30vw'}}>In doing so, your startup opens up a portion of its equity to retail investors, and your company can attract a more democratized pool of capital.</p>
+                                <button className="button nav">Learn More <FontAwesomeIcon icon={faLongArrowAltRight} /></button>
+                            </div>
                         </div>
-                        <img src={MainVectorGraphic} className="main image" />
+                    </div>
+                </div>
+                <div className="container content">
+                    <div className="container content centered">
+                        <div className="container content centered vertical">
+                            <div>
+                                <a className="text tagline">Generate a Token.</a>
+                                <p className="text tagline smaller" style={{width: '30vw'}}>Through our one-click platform, easily generate a token representing the equity your startup has partitioned.</p>
+                                <p className="text paragraph main" style={{width: '30vw'}}>
+                                    This is what enables your startup to be traded within our ecosystem. If public capital likes your startup, they will trade your token, and in turn, a portion of the transactions will be returned to your startup.
+                                </p>
+                                <button className="button nav">Learn More <FontAwesomeIcon icon={faLongArrowAltRight} /></button>
+                            </div>
+                        </div>
+                        <img src={TokenGenerationGraphic} className="secondary image" />
+                    </div>
+                </div>
+                <div className="container content">
+                    <div className="container content centered">
+                        <img src={SharePartitioningGraphic} className="secondary image" />
+                        <div className="container content centered vertical">
+                            <div style={{paddingLeft: '5vw'}}>
+                                <a className="text tagline">Discover Other Tokens.</a>
+                                <p className="text tagline smaller">
+                                    As an investor, discover new investment opportunities with massive potential.
+                                </p>
+                                <p className="text paragraph main" style={{width: '30vw'}}>
+                                    Using our marketplace, you can explore various startups that have listed equity on our platform, view their profiles, and invest in them if you feel they have potential.
+                                </p>
+                                <button className="button nav">Learn More <FontAwesomeIcon icon={faLongArrowAltRight} /></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="container content">
+                    <div className="container content centered">
+                        <div className="container content centered vertical">
+                            <div>
+                                <a className="text tagline">Grow your Portfolio.</a>
+                                <p className="text tagline smaller" style={{width: '30vw'}}>
+                                    Pick the best startups and watch your portfolio grow.
+                                </p>
+                                <p className="text paragraph main" style={{width: '30vw'}}>
+                                    As you invest in promising startups, your holdings will fluctuate depending on the trading volume of their respective tokens. If you manage to pick out good startups early, your portfolio will grow, and you can either hold until the company IPO's, or liquidate early.
+                                </p>
+                                <button className="button nav">Learn More <FontAwesomeIcon icon={faLongArrowAltRight} /></button>
+                            </div>
+                        </div>
+                        <img src={SharePartitioningGraphic} className="secondary image" />
                     </div>
                 </div>
             </div>
