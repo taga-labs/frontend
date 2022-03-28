ecd1a9c964072ac6abdd41d06f31a086583b4286
n-variable <nvariable@protonmail.com>
2022-03-28 12:08:46 -0400

    tailwind functional working on landing page refactor

--- src/components/app/landing/pages/about.js
+++ src/components/app/landing/pages/about.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function Home(props) {
+export default function About(props) {
     return(
         <div>
             
--- src/components/app/landing/pages/home.js
+++ src/components/app/landing/pages/home.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import Nav from '../../common/LandingNav';
 
 export default function Home(props) {
+    const [] = useState();
+
     return(
-        <div>
-            
+        <div className="container h-screen w-screen bg-slate-900 mx-auto">
+            <Nav />
         </div>
     )
 }
