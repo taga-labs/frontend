41c279fcb22548451ad76dfba40d9ac665c1b10a
n-variable <nvariable@protonmail.com>
2022-03-28 12:08:46 -0400

    tailwind functional working on landing page refactor

---
+++ src/components/app/landing/pages/registration.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Registration(props) {
+    return(
+        <div className="">
+
+        </div>
+    )
+}
---
+++ src/components/app/landing/pages/signin.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SignIn(props) {
+    return(
+        <div className="">
+
+        </div>
+    )
+}
