ad1b74a5a32a4ce9f995a67ab0358fb9cbaad862
n-variable <nvariable@protonmail.com>
2022-05-02 17:21:18 -0400

    backup

---
+++ src/pages/application/components/home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import AppNav from './general/nav';
+
+export default class Home extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+
+    }
+
+    render() {
+        return(
+            <div className="container overall">
+                <AppNav />
+                asd
+            </div>
+        )
+    }
+}
